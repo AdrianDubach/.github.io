76405d3b8298aac3e1c71423813703590a19019f
AdrianDubach <31879469+AdrianDubach@users.noreply.github.com>
2018-01-25 03:53:42 +0100

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,60 @@
+﻿let actualNrOfBulbs = 0;
+let currentTraversLight = 0;
+function changeState(state) {
+    if (state === "img/bulb-off.jpg") {
+        return state = "img/bulb-on.jpg";
+    }
+    else
+        return state = "img/bulb-off.jpg";
+}
+function lightSwitch(actuallight) {
+
+    let state = $(`#bulb${actuallight}`).attr("src")
+    state = changeState(state)
+    //andra argumentet sätter nya värdet.
+    $(`#bulb${actuallight}`).attr("src", state)
+}
+function bulbCss(x) {
+
+    $(".bulbs").attr("style", `width: ${100 / x}%; height: ${100 / x}%; max-width: 30%; max-height: 30%;`)
+}
+//----------BUTTONS-----------
+$("#addBulb").click(function () {
+
+    
+    actualNrOfBulbs++;
+    $("#imgContainer").append(`<img class="bulbs" id="bulb${actualNrOfBulbs}" src="img/bulb-off.jpg" />`)
+
+    bulbCss(actualNrOfBulbs)
+
+})
+
+
+$("#removeBulb").click(function () {
+
+
+    $(`#bulb${actualNrOfBulbs}`).remove();
+    if (actualNrOfBulbs > 0)
+    actualNrOfBulbs--;
+    if (currentTraversLight > actualNrOfBulbs) {
+        currentTraversLight = 0;
+    }
+    bulbCss(actualNrOfBulbs)
+
+})
+
+$("#traverse").click(function () {
+    lightSwitch(currentTraversLight)
+    currentTraversLight++;
+    if ((currentTraversLight % (actualNrOfBulbs + 1)) === 0 || currentTraversLight > actualNrOfBulbs) {
+        currentTraversLight = 1;
+    }
+    lightSwitch(currentTraversLight)
+   
+})
+
+$("#toggleAll").click(function () {
+    for (var i = 0; i < actualNrOfBulbs; i++) {
+        lightSwitch(i+1)
+    }
+})
